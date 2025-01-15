3f861d34c87a03e4f888c4a970f3f700f2ea33b8
gdjs2 <zhaoqi.xiao@gdjs2.cn>
2025-01-15 09:03:34 -0800

    fix: score acc

--- src/helpers/test-helpers.js
+++ src/helpers/test-helpers.js
@@ -10,11 +10,12 @@ const totalPercentageReducer = (acc, { score, weight, maxScore }) => {
 
 const getTestScore = (runnerResult) => {
   const { tests } = runnerResult;
-  const score = runnerResult.tests.reduce((acc, { status }) => {
-    return status === "pass" ? acc + 1 : acc;
+  const score = runnerResult.tests.reduce((acc, { score }) => {
+    acc += score;
+    return acc;
   }, 0);
 
-  return (score / tests.length) * (getMaxScoreForTest(runnerResult) || 0);
+  return score;
 };
 
 const getTestWeight = (maxScore, allMaxScores) => {
